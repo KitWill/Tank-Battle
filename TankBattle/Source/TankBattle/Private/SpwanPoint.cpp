67874b738cfd2c1176e98e9d7ecce958950cd316
KitWillGuy <c743047798@gmail.com>
2018-12-22 14:43:55 +0800

    Use SpawnActorDeferred Function And UGameplayStatics::FinishSpawningActor

--- TankBattle/Source/TankBattle/Private/SpwanPoint.cpp
+++ TankBattle/Source/TankBattle/Private/SpwanPoint.cpp
@@ -2,6 +2,7 @@
 
 #include "SpwanPoint.h"
 #include "Engine/World.h"
+#include "Kismet/GameplayStatics.h "
 
 // Sets default values for this component's properties
 USpwanPoint::USpwanPoint()
@@ -20,10 +21,12 @@ void USpwanPoint::BeginPlay()
 	Super::BeginPlay();
 
 	// ...
-	auto NewActor = GetWorld()->SpawnActor<AActor>(SpawnClass);
+	auto NewActor = GetWorld()->SpawnActorDeferred<AActor>(SpawnClass,GetComponentTransform());
+
 	if (!NewActor) return;
 
-	NewActor->AttachToComponent(this,FAttachmentTransformRules::KeepRelativeTransform);
+	NewActor->AttachToComponent(this,FAttachmentTransformRules::KeepWorldTransform);
+	UGameplayStatics::FinishSpawningActor(NewActor, GetComponentTransform());
 	
 }
 
